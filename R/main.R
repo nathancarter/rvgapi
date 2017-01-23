6826bd6a60b57492563b21b577aecc6395061597
Nathan Carter <ncarter@bentley.edu>
2017-01-23 16:24:55 -0500

    Completely rewrote everything to use R6 classes

--- R/main.R
+++ R/main.R
@@ -1,12 +1,3 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
 # Some useful keyboard shortcuts for package authoring:
 #
 #   Build and Reload Package:  'Cmd + Shift + B'
@@ -16,125 +7,124 @@
 library(curl)
 library(jsonlite)
 library(httpuv)
+library(R6)
 
-apiKey <- 'getoffmylawn'
-setApiKey <- function ( key ) {
-  apiKey <<- key
-}
-getHandle <- function ( otherOptions = list() ) {
-  handle <- new_handle()
-  handle_setheaders(
-    handle,
-    'X-API-KEY' = 'getoffmylawn',
-    'X-TITLE-ID' = 'semc-vainglory',
-    'Accept' = 'application/vnd.api+json',
-    .list = otherOptions )
-  handle
-}
+VGAPI <- R6Class(
+  'VGAPI',
+  public = list(
+    initialize = function ( bearer = 'aaa.bbb.ccc' ) {
+      private$handleOptions <- list()
+    },
+    setRegion = function ( region ) private$region <- region,
+    fetchMatches = function ( pageOffset = 0 ) {
+      private$fetchedMatches <- private$fetchData(
+        paste0( 'matches?', encodeURIComponent( 'page[offset]' ), '=', pageOffset ) )
+    },
+    count = function () length( private$fetchedMatches$data ),
+    getMatch = function ( index )
+      VGMatch$new( private$fetchedMatches$data[[index]], self ),
+    lookup = function ( id ) {
+      for ( included in private$fetchedMatches$included )
+        if ( included$id == id ) return( included )
+      return( NA )
+    }
+  ),
+  private = list(
+    rootUrl = 'https://api.dc01.gamelockerapp.com/',
+    region = 'na',
+    handleOptions = NULL,
+    fetchedMatches = NULL,
+    getHandle = function () {
+      handle <- new_handle()
+      handle_setheaders(
+        handle,
+        'Authorization' = 'Bearer aaa.bbb.ccc',
+        'X-TITLE-ID' = 'semc-vainglory',
+        'Accept' = 'application/vnd.api+json',
+        .list = private$handleOptions
+      )
+      handle
+    },
+    endPoint = function ( path )
+      paste0( private$rootUrl, 'shards/', private$region, '/', path ),
+    fetchData = function ( path )
+      fromJSON( rawToChar( curl_fetch_memory(
+        private$endPoint( path ), handle = private$getHandle() )$content ),
+        simplifyDataFrame = FALSE )
+  )
+)
 
-rootUrl <- 'https://api.dc01.gamelockerapp.com/'
-endPoint <- function ( path ) paste0( rootUrl, path )
-fetchData <- function ( path, handleOptions = list() )
-  fromJSON( rawToChar( curl_fetch_memory(
-    endPoint( path ), handle = getHandle( handleOptions ) )$content ),
-    simplifyDataFrame = FALSE )
+VGMatch <- R6Class(
+  'VGMatch',
+  public = list(
+    initialize = function ( match, apiObject ) {
+      private$match <- match
+      private$api <- apiObject
+    },
+    getTime = function ()
+      strptime( private$match$attributes$createdAt, '%Y-%m-%dT%H:%M:%SZ' ),
+    getDuration = function () private$match$attributes$duration,
+    getGameMode = function () private$match$attributes$gameMode,
+    getRegion = function () private$match$attributes$shardId,
+    getEndGameReason = function () private$match$attributes$stats$endGameReason,
+    getRoster = function ( index ) {
+      VGRoster$new(
+        private$lookup( private$match$relationships$rosters$data[[index]]$id ),
+        private$api )
+    },
+    cheater = function () private$fetchedMatches
+  ),
+  private = list(
+    match = NULL,
+    api = NULL,
+    lookup = function ( id ) private$api$lookup( id )
+  )
+)
 
-#' Fetching JSON data for many matches
-#'
-#' Fetches one page of matches from all available matches.
-#' Each match is a very large data structure.
-#' See the documentation for \code{getMatch()} for details.
-#'
-#' @param pageOffset defaults to 0, can be used to page through the huge list of matches
-#'
-#' @return A large data table converted from the JSON response with jsonlite
-#'
-#' @examples
-#' getMatches()
-#'
-#' @export
-getMatches <- function ( pageOffset = 0 ) {
-  jsonapi <- fetchData( paste0( 'matches?', encodeURIComponent( 'page[offset]' ), '=', pageOffset ) )
-  jsonapi
-}
+VGRoster <- R6Class(
+  'VGRoster',
+  public = list(
+    initialize = function ( roster, apiObject ) {
+      private$roster <- roster
+      private$api <- apiObject
+    },
+    getStats = function () private$roster$attributes$stats,
+    getMembers = function ()
+      lapply( private$roster$relationships$participants$data,
+              function ( participant )
+                VGPlayer$new( private$lookup( participant$id ), private$api ) ),
+    getTeamName = function () {
+      if ( !is.null( private$roster$relationships$team ) )
+        return( private$lookup( private$roster$relationships$team$data$id )$name )
+      else
+        return( NA )
+    },
+    cheater = function () private$roster
+  ),
+  private = list(
+    roster = NULL,
+    api = NULL,
+    lookup = function ( id ) private$api$lookup( id )
+  )
+)
 
-#' Fetching JSON data for one match
-#'
-#' Fetches the data for one match, given the ID for that match.
-#' This will be a large and complex data structure, described below.
-#'
-#' @param id the unique ID for the match, such as \code{'00cbe304-d7d7-11e6-ad79-062445d3d668'}
-#'
-#' @return A large data table converted from the JSON response with jsonlite.
-#'     \code{result$data$type} will be the type of object, usually \code{"match"}.
-#'     \code{result$data$id} will be the unique ID of the match, such as
-#'     \code{'00cbe304-d7d7-11e6-ad79-062445d3d668'}.
-#'     \code{result$data$attributes} is a list with various attributes of the match, including its
-#'     time, length, game mode, region, and how it ended.
-#'     \code{result$data$relationships} will be a list of two team rosters, each of which is just an
-#'     ID, of the same format as the match ID.
-#'     \code{result$included} is a table whose rows are players, participants, and rosters,
-#'     in no particular order.  The \code{type} column tells which of the three types the row holds.
-#'     The \code{id} column holds the unique ID for the object in that row.
-#'     The \code{attributes} column holds another data frame; its \code{$actor} column lists the
-#'     names of the heroes for each row that is a player, its \code{$stats} column has copious data
-#'     not documented here about activity throughout the match, and its \code{$name} column has the
-#'     in-game names for each row that is a player.
-#'
-#' @examples
-#' getMatch( '00cbe304-d7d7-11e6-ad79-062445d3d668' )
-#'
-#' @export
-getMatch <- function ( id ) fetchData( paste0( 'matches/', id ) )
-
-#' Fetching JSON data for many players
-#'
-#' This does not yet work; the API doesn't support it.  This stub is here for now.
-#'
-#' @return probably just a 404 error at this point
-#'
-#' @examples
-#' getPlayers()
-#'
-#' @export
-getPlayers <- function () fetchData( 'players' )
-
-#' Fetching JSON data for one player
-#'
-#' This does not yet work; the API doesn't support it.  This stub is here for now.
-#'
-#' @param id the unique ID for the player, such as \code{'83fc8814-b40a-11e6-8acb-06d90c28bf1a'}
-#'
-#' @return probably just a 404 error at this point
-#'
-#' @examples
-#' getPlayer( '83fc8814-b40a-11e6-8acb-06d90c28bf1a' )
-#'
-#' @export
-getPlayer <- function ( id ) fetchData( paste0( 'players/', id ) )
-
-#' Fetching JSON data for many teams
-#'
-#' This does not yet work; the API doesn't support it.  This stub is here for now.
-#'
-#' @return probably just a 404 error at this point
-#'
-#' @examples
-#' getTeams()
-#'
-#' @export
-getTeams <- function () fetchData( 'teams' )
-
-#' Fetching JSON data for one team
-#'
-#' This does not yet work; the API doesn't support it.  This stub is here for now.
-#'
-#' @param id the unique ID for the player, such as \code{'080a11a6-cfdd-11e6-90c5-0606daf7e67d'}
-#'
-#' @return probably just a 404 error at this point
-#'
-#' @examples
-#' getTeam( '080a11a6-cfdd-11e6-90c5-0606daf7e67d' )
-#'
-#' @export
-getTeam <- function ( id ) fetchData( paste0( 'teams/', id ) )
+VGPlayer <- R6Class(
+  'VGPlayer',
+  public = list(
+    initialize = function ( member, apiObject ) {
+      private$teamMember <- member
+      private$api <- apiObject
+      private$player <- private$lookup( member$relationships$player$data$id )
+    },
+    getName = function () private$player$attributes$name,
+    getStats = function () private$player$attributes$stats,
+    getHeroName = function () private$teamMember$attributes$actor,
+    getHeroStats = function () private$teamMember$attributes$stats
+  ),
+  private = list(
+    teamMember = NULL,
+    api = NULL,
+    player = NULL,
+    lookup = function ( id ) private$api$lookup( id )
+  )
+)
